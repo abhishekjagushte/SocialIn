d35adbbad2ec420692fe78fd588c8f6842a54067
abhishekjagushte <abhishekjagushte@gmail.com>
2020-03-14 01:05:06 +0530

    Abhishek Commit version 1

--- app/src/main/java/com/geforce/socialin/MainActivity.java
+++ app/src/main/java/com/geforce/socialin/MainActivity.java
@@ -1,8 +1,12 @@
 package com.geforce.socialin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()){
+                    case R.id.events:
+                        break;
+
+                    case R.id.people:
+                        break;
+
+                    case R.id.me:
+                        break;
+                }
+
+                return false;
+            }
+        });
+
     }
 }
